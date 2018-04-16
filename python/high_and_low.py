53cf3c35ef87725eb614dcc6362dfe9a6d30a1ff
john <john@lashl.ee>
2018-04-15 21:20:06 -0700

    Adding my first solution!

--- python/high_and_low.py
+++ python/high_and_low.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
 def high_and_low(numbers):
-    # ...
-    return numbers
+    ints = list(map(int, numbers.split()))
+    high_and_low_int = [sorted(ints)[i] for i in [-1,0]]
+    res = ' '.join(map(str,high_and_low_int))
+    return(res)
